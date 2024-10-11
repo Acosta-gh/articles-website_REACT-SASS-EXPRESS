56edde622e80471aabda30200527a2794144663f
Tu Nombre <cristian.acosta01@aol.com>
2024-10-10 23:34:04 -0300

    Creando la estructura básica para los archivos sass

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import { router } from "./router";
 import {RouterProvider} from "react-router-dom"
 
+import "./styles/main.scss";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>

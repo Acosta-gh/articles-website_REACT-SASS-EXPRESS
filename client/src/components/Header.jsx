dd00bb0b8d960c118fb45091041f169de086dd8d
Tu Nombre <cristian.acosta01@aol.com>
2024-10-12 02:27:25 -0300

    [200~Actualizó el Sass del header con mejoras en los estilos

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Hamburger from 'hamburger-react';
 import { Link } from 'react-router-dom';
+import { LuSearch } from "react-icons/lu";
+import { PiGitlabLogoSimpleFill } from "react-icons/pi";
 
 function Header() {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+    
+    const [isOpen, setOpen] = useState(false); // https://hamburger-react.netlify.app/
+
+    const searchRef = useRef(null);
+    const iconRef = useRef(null);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
+
+    const toggleSearch = () => { 
+        setIsSearchVisible(!isSearchVisible);
+    };
+
+    useEffect(() => {  // Handles clicks outside the search field and the search icon | Maneja clics fuera del campo de búsqueda y el ícono de búsqueda
+        const handleClickOutside = (event) => {
+            if (
+                searchRef.current && !searchRef.current.contains(event.target) &&
+                iconRef.current && !iconRef.current.contains(event.target)
+            ) {
+                setIsSearchVisible(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <header>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/articles">Articles</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+        <header className='header'>
+            <div className='header-burguer' onClick={toggleNav}>
+                <Hamburger toggled={isOpen} toggle={setOpen} size={25} />
+            </div>
+            <nav className={`header-nav ${isNavVisible ? 'visible' : ''}`}>
+                <Link to="/articles"><p>Our Articles</p></Link>
+                <Link to="/"><p>About Us</p></Link>
+                <Link to="/contact"><p>Contact Us</p></Link>
             </nav>
+            <input 
+                ref={searchRef}
+                className={`header-search ${isSearchVisible ? 'visible' : ''}`} 
+                type='text' 
+                placeholder='Search By...' 
+            />
+          
+            <div className='header-logo'>
+                <PiGitlabLogoSimpleFill />
+            </div>
+            <div 
+                className='header-search_icon' 
+                onClick={toggleSearch} 
+                ref={iconRef}
+            >
+                <LuSearch />
+            </div>
         </header>
     );
 }
